d289c585b498b965cbbd554e1f275521d83dacab
myjoshem <michellejmarkham@gmail.com>
2019-03-08 23:49:28 -0700

    fixed what eslint fixed, um, 'broke'

--- build/all.scripts.js
+++ build/all.scripts.js
@@ -45,10 +45,10 @@ document.getElementById("currentdate").innerHTML = fulldate;
 // thanks Bro Blazzard. Modified original code found at: https://codepen.io/blazzard-jason/pen/JNzKpY
 
 
-let currWeek = 9; //current week
-let week7 = 7;
-let week8 = 8; //for files completed in week 8, for files that will be static
-let week9 = 9; //this file will be updated/tweaked in week 9, unless it becomes part of a future assignment, only then the reference will change
+  let currWeek = 9; //current week
+  let week7 = 7;
+  let week8 = 8; //for files completed in week 8, for files that will be static
+  let week9 = 9; //this file will be updated/tweaked in week 9, unless it becomes part of a future assignment, only then the reference will change
 let updateHref = ['../lesson-' + week9 + '/index.html', '../lesson-' + currWeek + '/preston-' + currWeek + '.html', '#', '#', '../lesson-' + week8 + '/stormcenter.html', '../lesson-' + week7 + '/gallery-' + week7 + '.html'];
 
   document.getElementById("nav-home").href = updateHref[0];
